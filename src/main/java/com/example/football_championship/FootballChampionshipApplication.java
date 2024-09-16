6d6a139e82ce765d6ea35ee2f0cfcee43f0c336d
gmengqi <1014meng@gmail.com>
2024-09-16 19:34:54 +0800

    initial commit

---
+++ src/main/java/com/example/football_championship/FootballChampionshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.football_championship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballChampionshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballChampionshipApplication.class, args);
+	}
+
+}
