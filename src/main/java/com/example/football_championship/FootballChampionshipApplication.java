7367dc181ff4c1df6520bea2ebf5efc9c6acf788
gmengqi <1014meng@gmail.com>
2024-09-18 19:34:03 +0800

    add audit log

--- src/main/java/com/example/football_championship/FootballChampionshipApplication.java
+++ src/main/java/com/example/football_championship/FootballChampionshipApplication.java
@@ -2,8 +2,10 @@ package com.example.football_championship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FootballChampionshipApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/football_championship/audit/AuditLog.java
+++ src/main/java/com/example/football_championship/audit/AuditLog.java
@@ -1,2 +1,39 @@
-package com.example.football_championship.model;public class AuditLog {
+package com.example.football_championship.audit;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class AuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String action;
+    private String entityName;
+    private String entityId;
+    private String details;
+    private String performedBy="Admin";
+    private LocalDateTime timestamp;
+
+    // Getters and Setters
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public void setEntityName(String entityName) {
+        this.entityName = entityName;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public void setTimestamp(LocalDateTime dateTime) {
+        this.timestamp = dateTime;
+    }
 }
