6f16399c4ab4dce7d810259696c813f4ea10814f
gmengqi <1014meng@gmail.com>
2024-09-18 15:01:19 +0800

    add the dto for api

--- src/main/java/com/example/football_championship/DTO/UpdateTeamDTO.java
+++ src/main/java/com/example/football_championship/DTO/UpdateTeamDTO.java
@@ -1,2 +1,54 @@
-package com.example.football_championship.DTO;public class UpdateTeamDTO {
+package com.example.football_championship.DTO;
+
+import jakarta.persistence.Column;
+
+import java.time.LocalDate;
+
+public class UpdateTeamDTO {
+    private String teamName;
+    private String newName=null;
+    private LocalDate newRegistrationDate=null;
+    private Integer groupNumber=-1;
+    public Integer totalGoals=0;
+    public Integer matchPoints=0;
+    public Integer alternatePoints=0;
+    public Integer matchesPlayed=0;
+
+    // Getters and setters
+    public String getTeamName() {
+        return teamName;
+    }
+    public String getNewName() {
+        return newName;
+    }
+
+    public void setTeamName(String teamName) { this.teamName = teamName; }
+
+    public LocalDate getNewRegistrationDate() {
+        return newRegistrationDate;
+    }
+
+    public int getMatchesPlayed() {
+        return this.matchesPlayed;
+    }
+
+    public Integer getGroupNumber() {
+        return groupNumber;
+    }
+
+    public Integer getTotalGoals() {
+        return totalGoals;
+    }
+
+    public void setTotalGoals(int totalGoals) { this.totalGoals = totalGoals + this.totalGoals; }
+
+    public Integer getMatchPoints() {
+        return matchPoints;
+    }
+    public void setMatchPoints(int matchPoints) { this.matchPoints = matchPoints + this.matchPoints; }
+
+    public Integer getAlternatePoints() {
+        return alternatePoints;
+    }
+
 }
