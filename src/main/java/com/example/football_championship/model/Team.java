5b01fb0e69367fb3b3479f83a4eab345e646224c
gmengqi <1014meng@gmail.com>
2024-09-18 14:59:28 +0800

    add the models

--- src/main/java/com/example/football_championship/model/Team.java
+++ src/main/java/com/example/football_championship/model/Team.java
@@ -1,2 +1,82 @@
-package com.example.football_championship.model;public class Team {
+package com.example.football_championship.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "t_team_entity")
+public class Team extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false)
+    public String name;
+
+    @Column(nullable = false)
+    public LocalDate registrationDate;
+
+    @Column(nullable = false)
+    public int groupNumber;
+
+    public int totalGoals = 0;
+    public int matchPoints = 0;
+    public int alternatePoints = 0;
+    public int matchesPlayed = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMatchesPlayed(int matchesPlayed) {
+        this.matchesPlayed = matchesPlayed;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public int getGroupNumber() {
+        return groupNumber;
+    }
+
+    public void setGroupNumber(int groupNumber) {
+        this.groupNumber = groupNumber;
+    }
+
+    public int getTotalGoals() {
+        return totalGoals;
+    }
+
+    public void setTotalGoals(int totalGoals) {
+        this.totalGoals = totalGoals;
+    }
+
+    public int getMatchPoints() {
+        return matchPoints;
+    }
+
+    public void setMatchPoints(int matchPoints) {
+        this.matchPoints = matchPoints;
+    }
+
+    public int getAlternatePoints() {
+        return alternatePoints;
+    }
+
+    public void setAlternatePoints(int alternatePoints) {
+        this.alternatePoints = alternatePoints;
+    }
+
+    public int getTotalMatchPoints() {
+        return this.matchPoints;
+    }
 }
