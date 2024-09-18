b9d50be11eb4900f0e01180e23cdaded4ea48c26
gmengqi <1014meng@gmail.com>
2024-09-18 15:01:38 +0800

    add response dto

--- src/main/java/com/example/football_championship/DTO/TeamProcessingResult.java
+++ src/main/java/com/example/football_championship/DTO/TeamProcessingResult.java
@@ -1,2 +1,27 @@
-package com.example.football_championship.DTO;public class TeamProcessingResult {
+package com.example.football_championship.DTO;
+
+import com.example.football_championship.model.Team;
+
+import java.util.List;
+
+public class TeamProcessingResult {
+    private List<Team> validTeams;
+    private List<String> errors;
+
+    // Getters and setters for serialization
+    public List<Team> getValidTeams() {
+        return validTeams;
+    }
+
+    public void setValidTeams(List<Team> validTeams) {
+        this.validTeams = validTeams;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<String> errors) {
+        this.errors = errors;
+    }
 }
