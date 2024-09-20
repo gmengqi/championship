43bc75e2913c6932a3674f850076db89ca34dd40
gmengqi <1014meng@gmail.com>
2024-09-20 23:07:31 +0800

    update webconfig

--- src/main/java/com/example/football_championship/config/WebConfig.java
+++ src/main/java/com/example/football_championship/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://football-frontend-two.vercel.app")  // Allow requests from React frontend
+                .allowedOrigins("https://football-frontend-two.vercel.app", "http://localhost:3000/")  // Allow requests from React frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type", "Accept")
                 .allowCredentials(true);
